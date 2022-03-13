552b3735a383b8de43e599c4ff88fbcdc002cec4
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-03-13 18:10:29 +0530

    infra: react-query addded

--- packages/client/pages/_app.js
+++ packages/client/pages/_app.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
