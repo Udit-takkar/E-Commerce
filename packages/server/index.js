9d127eb37ccf42003c67bd5e331829af1953da81
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-03-18 15:07:24 +0530

    fix: cors on live website

--- packages/server/index.js
+++ packages/server/index.js
@@ -22,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
