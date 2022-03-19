d9bb44f2a86fc3f7021b360b5708fd97cd79c817
Yash Gupta <yash3144@gmail.com>
2022-03-19 23:54:38 +0530

    commit code

--- packages/client/pages/dashboard/SellerDashboard/MainData.js
+++ packages/client/pages/dashboard/SellerDashboard/MainData.js
@@ -4,8 +4,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-unresolved */
-import { useEffect } from 'react';
-import Chart from 'chart.js/auto';
+// import { useEffect } from 'react';
+// import Chart from 'chart.js/auto';
 import { Doughnut, Line, Pie, Bar } from 'react-chartjs-2';
 // import { getAdminProducts } from '../../actions/productAction';
 // import { useSelector, useDispatch } from 'react-redux';
