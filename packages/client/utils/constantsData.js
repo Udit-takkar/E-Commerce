f832301943175a3419f4a7f415065171d3b91a14
Yash Gupta <yash3144@gmail.com>
2022-03-19 23:39:07 +0530

    Dummy data and helper function

---
+++ packages/client/utils/constantsData.js
@@ -0,0 +1,348 @@
+export const categories = [
+  'Cotton Handloom Fabrics',
+  'Handloom Carpets',
+  'Dupattas',
+  'Handloom Carpets',
+  'Tussar Silk Dupatta',
+  'Buddha Statue',
+];
+
+export const ordersData = [
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 999,
+    createdAt: '2022-03-09T09:33:04.078+00:00',
+    orderStatus: 'Delivered',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+    orderStatus: 'Delivered',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2020-03-09T09:33:04.078+00:00',
+    orderStatus: 'Shipped',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2022-03-09T09:33:04.078+00:00',
+    orderStatus: 'Processing',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+    orderStatus: 'Shipped',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2020-03-09T09:33:04.078+00:00',
+    orderStatus: 'Processing',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2022-03-09T09:33:04.078+00:00',
+    orderStatus: 'Processing',
+  },
+  {
+    _id: 1,
+    itemsQty: 3,
+    amount: 10000,
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+    orderStatus: 'Delivered',
+  },
+];
+
+export const productsData = [
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Dupattas',
+    stock: 100,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Dupattas',
+    stock: 101,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Cotton Handloom Fabrics',
+    stock: 10,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Cotton Handloom Fabrics',
+    stock: 1,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Cotton Handloom Fabrics',
+    stock: 105,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 200,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Handloom Carpets',
+    stock: 400,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Buddha Statue',
+    stock: 300,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Indian Handicrafts',
+    stock: 100,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Buddha Statue',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Tussar Silk Dupatta',
+    stock: 100,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Indian Handicrafts',
+    stock: 299,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+  {
+    _id: 1,
+    name: 'Indian Traditional Handicraft, For Decorative, 12',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/11/BQ/CO/YY/77706935/p9-500x500.png',
+    category: 'Tussar Silk Dupatta',
+    stock: 0,
+    price: '390.0',
+    cprice: '300',
+    rating: '5',
+  },
+];
+
+export const usersData = [
+  {
+    _id: 1,
+    name: 'Rahul Sharma',
+    avatar: {
+      url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+    },
+    email: 'rahul21@gmail.com',
+    gender: 'Male',
+    role: 'Buyer',
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+  },
+  {
+    _id: 1,
+    name: 'Rahul Sharma',
+    avatar: {
+      url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+    },
+    email: 'rahul21@gmail.com',
+    gender: 'Male',
+    role: 'Buyer',
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+  },
+  {
+    _id: 1,
+    name: 'Vishu Sharma',
+    avatar: {
+      url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+    },
+    email: 'VishuSharma1@gmail.com',
+    gender: 'Male',
+    role: 'Buyer',
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+  },
+  {
+    _id: 1,
+    name: 'Rahul Sharma',
+    avatar: {
+      url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+    },
+    email: 'rahul21@gmail.com',
+    gender: 'Male',
+    role: 'Buyer',
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+  },
+  {
+    _id: 1,
+    name: 'Neha Bhatia',
+    avatar: {
+      url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+    },
+    email: 'NehaBhatia@gmail.com',
+    gender: 'Male',
+    role: 'Buyer',
+    createdAt: '2021-03-09T09:33:04.078+00:00',
+  },
+];
+
+export const reviewsData = [
+  {
+    _id: 1,
+    rating: 4,
+    comment: 'Nice Product!!',
+    name: 'Rahul Sharma',
+  },
+  {
+    _id: 2,
+    rating: 3,
+    comment: 'Product is not upto my expectations.',
+    name: 'Neha Bhatia',
+  },
+  {
+    _id: 3,
+    rating: 5,
+    comment: 'Nice Product!!',
+    name: 'Vishu Sharma',
+  },
+  {
+    _id: 4,
+    rating: 5,
+    comment: 'Nice Product!!',
+    name: 'Bhavy Koli',
+  },
+  {
+    _id: 3,
+    rating: 4,
+    comment: 'This painting is very beautiful',
+    name: 'Sachin dubey',
+  },
+];
