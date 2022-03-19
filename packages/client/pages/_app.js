481c218d3e115bbab05ba99e5209dcefb105ab07
Yash Gupta <yash3144@gmail.com>
2022-03-19 19:16:02 +0530

    Wrap notistick around whole app

--- packages/client/pages/_app.js
+++ packages/client/pages/_app.js
@@ -4,19 +4,22 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
 import Header from '../components/Header';
+import { SnackbarProvider } from 'notistack';
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Hydrate>
-    </QueryClientProvider>
+    <SnackbarProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
+      </QueryClientProvider>
+    </SnackbarProvider>
   );
 }
 
