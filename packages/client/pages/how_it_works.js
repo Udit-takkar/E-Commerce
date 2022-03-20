947f177de9234bdb4374a2e3fa77c80fa99523bf
Yash Gupta <yash3144@gmail.com>
2022-03-20 19:30:43 +0530

    how it works page

---
+++ packages/client/pages/how_it_works.js
@@ -0,0 +1,214 @@
+/* eslint-disable react/self-closing-comp */
+import Image from 'next/image';
+
+export default function HowItWorks() {
+  return (
+    <div className="min-h-screen mt-20 sm:min-w-full">
+      <div className="bg-white border-b py-8">
+        <div className=" max-w-5xl mx-auto m-8">
+          <div style={{ display: 'flex' }}>
+            <div className="sm:w-1/2">
+              <h3 className="text-3xl text-gray-800 font-bold mb-8">
+                Video Commerce Changes Everything for Online Shopping
+              </h3>
+              <p className="text-gray-600 mb-8">
+                <ul className="list-unstyled">
+                  <li className="mb-0">
+                    ✅ Live video & virtual shopping supercharges your existing
+                    acquisition funnel- create awareness, increase conversion
+                    rates and retain customers.
+                    <br />
+                  </li>
+                  <li className="mb-0">
+                    ✅ Whether you want to support your build community with
+                    live streams or sell products with 1-to-1 virtual shopping -
+                    we've got you covered.
+                  </li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </div>
+            <div className="sm:w-1/2 ml-40">
+              <Image
+                src="https://www.singlegrain.com/wp-content/uploads/2019/07/SG-What-Is-Live-Stream-Shopping.jpg"
+                height="400"
+                width="400"
+              />
+            </div>
+          </div>
+          <div style={{ display: 'flex' }} className="mt-32">
+            <div className="sm:w-1/2" style={{ marginTop: '-2rem' }}>
+              <Image
+                src="https://typof.com/site/images/home3.webp"
+                height="400"
+                width="400"
+              />
+            </div>
+            <div className="sm:w-1/2">
+              <h3 className="text-3xl text-gray-800 font-bold mb-3">
+                Acquire new audience and grow your business with Ecommerce
+                Marketing tools.
+              </h3>
+              <p className="text-gray-600 mb-8">
+                <ul className="list-unstyled">
+                  <li className="mb-0">
+                    ✅ Get an edge over competition by getting connected to your
+                    {'  '}audience directly and instantly.
+                    <br />
+                  </li>
+                  <li className="mb-0">
+                    ✅ Engage with your customer in real time.
+                  </li>
+
+                  <li className="mb-0">✅ Sell your product instantly.</li>
+                  <li className="mb-0">
+                    ✅ Save time and money by using discounts and coupons to
+                    increase sales.
+                  </li>
+
+                  <li className="mb-0">✅ Increase loyal customers.</li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </div>
+          </div>
+          <div style={{ display: 'flex' }} className="mt-32">
+            <div className="sm:w-1/2">
+              <h3 className="text-3xl text-gray-800 font-bold mb-3">
+                Manage your Inventory, Daily Orders, Fulfillment etc. all in one
+                place
+              </h3>
+              <p className="text-gray-600 mb-8">
+                <ul className="list-unstyled">
+                  <li className="mb-0">
+                    ✅ Advanced reporting and analytics. Automated Inventory
+                    management.
+                    <br />
+                  </li>
+                  <li className="mb-0">
+                    ✅ Fulfillment, Inventory, Shipping, Payments and Accounting
+                    all in one place.
+                  </li>
+
+                  <li className="mb-0">
+                    ✅ Takes all the stress out of running your online business.
+                  </li>
+                  <li className="mb-0">
+                    ✅ Keeps your Business simple, organized and running like
+                    clockwork.
+                  </li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </div>
+            <div className="sm:w-1/2 ml-40" style={{ marginTop: '-4rem' }}>
+              <Image
+                src="https://typof.com/site/images/home2.webp"
+                height="700"
+                width="700"
+              />
+            </div>
+          </div>
+          <div style={{ display: 'flex' }} className="mt-32">
+            <div className="sm:w-1/2">
+              <Image
+                src="https://media.istockphoto.com/vectors/influencer-marketing-potential-product-buyers-or-consumer-products-vector-id1187679239?k=20&m=1187679239&s=612x612&w=0&h=BNqBkdX7sNbDvBW6Wvi4YocpCUH2DCll7iHEF6frDvk="
+                height="400"
+                width="400"
+              />
+            </div>
+            <div className="sm:w-1/2">
+              <h3 className="text-3xl text-gray-800 font-bold mb-3">
+                Micro Influencer: When Smaller is better to increase sales
+              </h3>
+              <p className="text-gray-600 mb-8">
+                <ul className="list-unstyled">
+                  <li className="mb-0">
+                    ✅ What are micro-influencers ?
+                    <br />
+                    Micro-influencers are pretty much exactly what they sound
+                    like. They are hyper-specific influencers who have lower
+                    follower counts in a given niche. They have intensely
+                    engaged audiences, yet a relatively low (below 25k) follower
+                    count.
+                  </li>
+                  <li className="mb-0">
+                    ✅ Influencer marketing is one of the best ways to drive
+                    sales.
+                    <br />
+                  </li>
+
+                  <li className="mb-0">
+                    ✅ They helps people bridge the gap between a product and
+                    developing the need for that product in their daily lives!
+                  </li>
+                  <li className="mb-0">
+                    ✅ On our platform, whenever they like any products. They
+                    can promote that products on their social media websites.
+                    And In return, they get the commission.
+                  </li>
+                  <li className="mb-0">
+                    ✅ In this way, we are helping sellers by increasing their
+                    sales as well as influencers with less followers to earn
+                    money.
+                  </li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </div>
+          </div>
+          <div style={{ display: 'flex' }} className="mt-32">
+            <div className="sm:w-1/2">
+              {/* <p className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xl font-bold leading-none text-red-100 bg-red-600 rounded-lg mb-4">
+                STEP 1
+              </p> */}
+              <h3 className="text-3xl text-gray-800 font-bold mb-3">
+                Earn Money Onlne while working from home. Start Reselling Today
+              </h3>
+              <p className="text-gray-600 mb-8">
+                <ul className="list-unstyled">
+                  <li className="mb-0">
+                    ✅ Reselling is a way of running your own business with zero
+                    investment.
+                    <br />
+                  </li>
+                  <li className="mb-0">
+                    ✅ As a Reseller, you share products listed on the app
+                    either with your network or on social media and earn a
+                    profit with every sale.
+                  </li>
+
+                  <li className="mb-0">
+                    ✅ There are types categories of product on the app through
+                    which you can browse.
+                  </li>
+                  <li className="mb-0">
+                    ✅ The products are provided by sellers, who have a tie-up
+                    with our app.
+                  </li>
+                  <li className="mb-0">
+                    ✅ Reselling is an easy way of working from home and earning
+                    huge profits.
+                  </li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </div>
+            <div className="sm:w-1/2 ml-40">
+              <Image
+                src="https://img.freepik.com/free-vector/online-payment-security-via-smartphone-two-man-hand-shake-reseller-payment-flat-illustration-s_210682-198.jpg?w=1380"
+                height="500"
+                width="700"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
